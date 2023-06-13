eca082d5957d5f4f6072ed9ee709e7cddc6d6d24
Acryt <acryter@gmail.com>
2023-06-13 19:56:12 +0300

    JS all-author Refactored

--- app/themes/uggwc_pro/parts/section-all-author.php
+++ app/themes/uggwc_pro/parts/section-all-author.php
@@ -15,119 +15,116 @@
 </section>
 
 <script>
-	// JavaScript код для контейнера карточек авторов
-	const lmCont = document.querySelector('#aauthor-lm-c');
-	const lmBtn = document.querySelector('#aauthor-lm-btn');
-	// Массив с карточками авторов
-	const authors = <?php echo json_encode(carbon_get_theme_option('cf_author')); ?>;
+// JavaScript код для контейнера карточек авторов
+const lmCont = document.querySelector("#aauthor-lm-c");
+const lmBtn = document.querySelector("#aauthor-lm-btn");
+// Массив с карточками авторов
+const authors = <?php echo json_encode(carbon_get_theme_option('cf_author')); ?>;
 
-	// Функция для создания HTML разметки карточки автора
-	function createAuthorCard(author) {
-		let rating = author.cf_author_rating;
-		let ratingScore = rating / 10;
-		let solidStar = Math.floor(rating / 10);
-		let emptyStar = 5 - solidStar - Math.floor(rating / 5) % 2;
-		let cardHtml = `<div class="aauthor__item card shadow">
-				<div class="aauthor__photo"><img src="${author.cf_author_photo}" alt="photo"></div>
-				<div class="aauthor__cont">
-					<div class="aauthor__one">
-						<h6 class="aauthor__name">${author.cf_author_name}</h6>
-						<div class="aauthor__stars">`;
+// Функция для создания HTML разметки карточки автора
+function createAuthorCard(author) {
+	let rating = author.cf_author_rating;
+	let ratingScore = rating / 10;
+	let solidStar = Math.floor(rating / 10);
+	let emptyStar = 5 - solidStar - (Math.floor(rating / 5) % 2);
+	let cardHtml = `<div class="aauthor__item card shadow">
+        <div class="aauthor__photo"><img src="${author.cf_author_photo}" alt="photo"></div>
+        <div class="aauthor__cont">
+          <div class="aauthor__one">
+            <h6 class="aauthor__name">${author.cf_author_name}</h6>
+            <div class="aauthor__stars">`;
 
-		for (let i = 0; i < solidStar; i++) {
-			cardHtml += `<i class="fa-solid fa-star"></i>`;
-		}
-		if (Math.floor(rating / 5) % 2 !== 0) {
-			cardHtml += `<i class="fa-solid fa-star-half-stroke"></i>`;
-		}
-		for (let i = 0; i < emptyStar; i++) {
-			cardHtml += `<i class="fa-regular fa-star"></i>`;
-		}
-		cardHtml += `</div>
-					</div>
-					<div class="aauthor__two">
-						<div>Rezension: <span>${author.cf_author_review}</span></div>
-						<div>Grad: <span>${author.cf_author_quality}</span></div>
-						<div>Aufträge insgesamt: <span>${author.cf_author_orders}</span></div>
-						<div>Erfolgsrate: <span>${author.cf_author_percent}</span></div>
-					</div>
-					<a class="btn fit borda js_author-btn" data-slr=".popup__bigform"><span>PREIS KALKULIEREN</span></a>
-					<hr>
-					<div class="aauthor__three">
-						<div><strong>Wettkämpfe: </strong></div>`;
+	for (let i = 0; i < solidStar; i++) {
+		cardHtml += `<i class="fa-solid fa-star"></i>`;
+	}
+	if (Math.floor(rating / 5) % 2 !== 0) {
+		cardHtml += `<i class="fa-solid fa-star-half-stroke"></i>`;
+	}
+	for (let i = 0; i < emptyStar; i++) {
+		cardHtml += `<i class="fa-regular fa-star"></i>`;
+	}
+	cardHtml += `</div>
+          </div>
+          <div class="aauthor__two">
+            <div>Rezension: <span>${author.cf_author_review}</span></div>
+            <div>Grad: <span>${author.cf_author_quality}</span></div>
+            <div>Aufträge insgesamt: <span>${author.cf_author_orders}</span></div>
+            <div>Erfolgsrate: <span>${author.cf_author_percent}</span></div>
+          </div>
+          <a class="btn fit borda aut_btn" data-slr=".popup__bigform"><span>PREIS KALKULIEREN</span></a>
+          <hr>
+          <div class="aauthor__three">
+            <div><strong>Wettkämpfe: </strong></div>`;
 
-		getCompetitionValues(author.cf_author_competition).forEach(el => {
-			cardHtml += `<div>${el}</div>`;
+	getCompetitionValues(author.cf_author_competition).forEach((el) => {
+		cardHtml += `<div>${el}</div>`;
+	});
+
+	cardHtml +=
+		`</div>
+          <details class="aauthor__det">
+            <summary class="aauthor__sum">
+              <h6>Beschreibung:</h6>
+            </summary>
+            <div class="aauthor__desc">` +
+		author.cf_author_desc +
+		`</div>
+          </details>
+        </div>
+      </div>`;
+	return cardHtml;
+}
+
+function getCompetitionValues(ids) {
+	let values = [];
+	let arr_select = <?php echo json_encode(carbon_get_theme_option('cf_select_competition')); ?>;
+	ids.forEach((el) => {
+		arr_select.forEach((item) => {
+			if (item.cf_select_competition_id == el) {
+				values.push(item.cf_select_competition_value);
+			}
 		});
+	});
+	return values;
+}
 
-		cardHtml += `</div>
-					<details class="aauthor__det">
-						<summary class="aauthor__sum">
-							<h6>Beschreibung:</h6>
-						</summary>
-						<div class="aauthor__desc">` + author.cf_author_desc + `</div>
-					</details>
-				</div>
-			</div>`;
-		return cardHtml;
-	}
+// Функция для отображения карточек авторов на странице
+function renderAuthorCards(cards) {
+	const cardsHtml = Array.from(cards).map(createAuthorCard).join("");
+	lmCont.innerHTML = cardsHtml;
+}
 
-	function getCompetitionValues(ids) {
-		let values = [];
-		let arr_select = <?php echo json_encode(carbon_get_theme_option('cf_select_competition')); ?>;
-		ids.forEach(el => {
-			arr_select.forEach(item => {
-				if (item.cf_select_competition_id == el) {
-					values.push(item.cf_select_competition_value);
-				}
-			})
-		});
-		return values;
-	}
-
-	// Функция для отображения карточек авторов на странице
-	function renderAuthorCards(cards) {
-		const cardsHtml = cards.map(createAuthorCard).join('');
-		lmCont.innerHTML = cardsHtml;
-	}
-	function evLis(el) {
-		document.querySelector(el.dataset.slr).classList.add('_active');
+// Функция для обработки клика на контейнере карточек авторов
+function handleAuthorClick(event) {
+	// Проверяем, что клик был на кнопке "PREIS KALKULIEREN"
+	if (event.target.closest('.aut_btn').classList.contains('aut_btn')) {
+		// Получаем ближайшую карточку автора
+		let card = event.target.closest(".aut_btn");
+		// Делаем что-то с карточкой (например, открываем попап)
+		document.querySelector(card.dataset.slr).classList.add('_active');
 		document.querySelector('.popups').classList.add('_active');
 	}
-	// Обработчик клика на кнопку "Загрузить еще"
-	lmBtn.addEventListener('click', function () {
-		// Добавляем 3 карточки авторов из массива к текущим карточкам
-		const startIndex = lmCont.children.length;
-		const endIndex = startIndex + 3;
-		const newCards = authors.slice(startIndex, endIndex);
-		const newCardsHtml = newCards.map(createAuthorCard).join('');
+}
 
-		let buttons = document.querySelectorAll('.js_author-btn');
-		buttons.forEach(el => {
-			el.removeEventListener('click', function() {
-				evLis(el);
-			});
-		});
+// Обработчик клика на кнопку "Загрузить еще"
+lmBtn.addEventListener("click", function () {
+	// Добавляем 3 карточки авторов из массива к текущим карточкам
+	const startIndex = lmCont.children.length;
+	const endIndex = startIndex + 3;
+	const newCards = authors.slice(startIndex, endIndex);
+	const newCardsHtml = newCards.map(createAuthorCard).join("");
 
-		lmCont.insertAdjacentHTML('beforeend', newCardsHtml);
-		// Загрузить еще"
-		if (endIndex >= authors.length) { lmBtn.style.display = 'none'; }
+	lmCont.insertAdjacentHTML("beforeend", newCardsHtml);
+	// Загрузить еще"
+	if (endIndex >= authors.length) {
+		lmBtn.style.display = "none";
+	}
+});
 
-		buttons = document.querySelectorAll('.js_author-btn');
+// Отображаем первые 5 карточек авторов при загрузке страницы
+renderAuthorCards(authors.slice(0, 3));
 
-		buttons.forEach(el => {
-			el.addEventListener('click', function() {
-				evLis(el);
-			});
-		});
-	});
-	// Отображаем первые 5 карточек авторов при загрузке страницы
-	renderAuthorCards(authors.slice(0, 3));
-	const buttons = document.querySelectorAll('.js_author-btn');
-	Array.from(buttons).forEach(el => {
-		el.addEventListener('click', function evLis(event) {
-			document.querySelector(el.dataset.slr).classList.add('_active');
-			document.querySelector('.popups').classList.add('_active');
-		});
-	});
+// Навешиваем обработчик клика на контейнер карточек авторов
+lmCont.addEventListener("click", handleAuthorClick);
+
 </script>
