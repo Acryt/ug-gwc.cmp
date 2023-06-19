fc694c926900ad2d93a7863835c6f5164c6ad65c
Acryt <acryter@gmail.com>
2023-06-19 18:03:51 +0300

    404

--- app/themes/uggwc_pro/404.php
+++ app/themes/uggwc_pro/404.php
@@ -8,15 +8,29 @@ Template Name: 404
 <?php get_header(); ?>
 
 <main class="main">
-	<?php get_template_part('parts/section-first'); ?>
-	<section id="content" class="section content">
-		<div class="wrapper">
-			<div class="page-content">
-				<p>
-					<?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'twentytwentyone'); ?>
-				</p>
-				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
+	<section id="notfound" class="section notfound">
+		<div class="wrapper notfound__container">
+			<div class="notfound__header">
+				<h1>Oops... <br> Seite nicht gefunden</h1>
+			</div>
+			<div class="notfound__content">
+				<div class="notfound__text content">
+					<p>Leider ist die von Ihnen gesuchte Seite aus einem der folgenden Gründe nicht mehr verfügbar:</p>
+					<ul>
+						<li>Die Seite wurde wegen mangelnder Relevanz entfernt</li>
+						<li>Die Seite wurde verlagert</li>
+						<li>Sie haben vielleicht ein paar Buchstaben in Ihrer Adresse vergessen (das passiert uns auch manchmal).</li>
+					</ul>
+					<p>Sie können zur <a href="/">Hauptseite</a> zurückkehren und die Suche erneut starten.</p>
+					<br><br>
+					<p>Wenn Sie Fragen an uns haben, kontaktieren Sie uns bitte unter <br><a href="mailto:info@ug-gwc.de">info@ug-gwc.de</a></p>
+					<br><br>
+					<a class="btn wave_effect js_btn fit" data-slr=".popup__bigform"><span>PREIS KALKULIEREN</span></a>
+				</div>
+				<div class="notfound__img">
+					<img src="<?php echo get_bloginfo('template_url'); ?>/assets/images/first/notfound.svg" alt="not found">
+				</div>
+			</div>
 		</div>
 	</section>
 </main>
