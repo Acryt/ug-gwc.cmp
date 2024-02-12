2560bde7e854c32382d4d648e58a9b2a8e120994
Acryt <acryter@gmail.com>
2024-02-12 16:48:54 +0300

    impressum +rocket

--- app/themes/uggwc_pro/templates/impressum.php
+++ app/themes/uggwc_pro/templates/impressum.php
@@ -9,17 +9,17 @@ get_header();
 ?>
 
 <main class="main">
-	<?php get_template_part('parts/section-impressum'); ?>
-	
-	<?php get_template_part('parts/section-card-manager'); ?>
 	<?php
+	get_template_part('parts/section-impressum');
+
+	get_template_part('parts/section-busines');
+	get_template_part('parts/section-care');
+	get_template_part('parts/section-card-manager');
 	get_template_part('parts/section-card-client');
 	// get_template_part('parts/section-card-author');
 	get_template_part('parts/section-card-dev');
 	get_template_part('parts/section-card-recruit');
 	get_template_part('parts/section-card-boss');
-	get_template_part('parts/section-care');
-	get_template_part('parts/section-busines');
 	get_template_part('parts/section-faq-accrd');
 	get_template_part('parts/section-form');
 	?>
