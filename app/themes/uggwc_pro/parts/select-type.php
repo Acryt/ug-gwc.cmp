2a0575ec509168bf02b3638abd53f9313db5bc7d
Acryt <acryter@gmail.com>
2024-04-02 13:02:59 +0300

    selects

--- app/themes/uggwc_pro/parts/select-type.php
+++ app/themes/uggwc_pro/parts/select-type.php
@@ -1,12 +1,14 @@
-<div class="form_select form__type req_star" data-state="" data-tippy-content="Wenn Sie Ihren Arbeitstyp nicht in der Liste gefunden haben, wählen Sie 'Sonstige Arbeit' aus.">
+<div class="form_select form__type req_star" data-state=""
+	data-tippy-content="Wenn Sie Ihren Arbeitstyp nicht in der Liste gefunden haben, wählen Sie 'Sonstige Arbeit' aus.">
 	<div class="form__icon"><i class="fa-solid fa-list"></i></div>
 	<select class="search-select wide select_online type" name="type" required="">
 		<option value="" disabled="" selected="" class="">Arbeitstyp</option>
-		<?php
-		foreach (TYPES as $value) { ?>
-			<option value="<?php echo $value; ?>" class="">
-				<?php echo $value; ?>
-			</option>
-		<?php } ?>
+		<?php if (TYPES != null) {
+			foreach (TYPES as $value) { ?>
+				<option value="<?php echo $value; ?>" class="">
+					<?php echo $value; ?>
+				</option>
+			<?php }
+		} ?>
 	</select>
 </div>
