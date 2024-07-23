d4f704dae776a2889c8b347ac15e184428a87a48
Acryt <acryter@gmail.com>
2024-07-23 16:58:16 +0200

    google map translate

--- app/themes/uggwc_pro/parts/section-busines.php
+++ app/themes/uggwc_pro/parts/section-busines.php
@@ -6,7 +6,7 @@
 		</div>
 		<div class="section__content">
 			<div class="busines__map card shadow">
-				<iframe class="g_maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2431.007538939913!2d13.36077837732656!3d52.46089090351477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a850386fc4f675%3A0xc413a32cd0675fd2!2sUG%20GWC!5e0!3m2!1sru!2sby!4v1681746084597!5m2!1sru!2sby" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<iframe class="g_maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2431.0075037620404!2d13.362182776994052!3d52.460891540603704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a850386fc4f675%3A0xc413a32cd0675fd2!2sUG%20GWC!5e0!3m2!1sde!2spl!4v1721745928312!5m2!1sde!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 			</div>
 			<?php get_template_part('parts/swiper-photo'); ?>
 			<div class="busines__text card shadow">
